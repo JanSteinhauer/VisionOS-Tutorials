e3d25f394509c34a7276d1ec48d1fb6693c55d8c
Jan Steinhauer <jan.steinhauer@sap.com>
2025-03-29 22:13:31 +0100

    added 360 Image VisionOS

---
+++ 360 Image VisionOS/360 Image VisionOS/_60_Image_VisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  _60_Image_VisionOSApp.swift
+//  360 Image VisionOS
+//
+//  Created by Steinhauer, Jan on 29.03.25.
+//
+
+import SwiftUI
+
+@main
+struct _60_Image_VisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 360 Image VisionOS/360 Image VisionOSTests/_60_Image_VisionOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  _60_Image_VisionOSTests.swift
+//  360 Image VisionOSTests
+//
+//  Created by Steinhauer, Jan on 29.03.25.
+//
+
+import Testing
+@testable import _60_Image_VisionOS
+
+struct _60_Image_VisionOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
